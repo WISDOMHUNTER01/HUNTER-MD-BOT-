ff5f0e6efa6820a0ca97e6df11cfbafd6efe2fca
joeljamesXtech <joeljamestech40@gmail.com>
2025-05-11 03:13:03 +0300

    HUNTER-MD-BOT

--- framework/handler.js
+++ framework/handler.js
@@ -1,8 +1,8 @@
-import { serialize, decodeJid } from '../../lib/Serializer.js';
+import { serialize, decodeJid } from '../lib/Serializer.js';
 import path from 'path';
 import fs from 'fs/promises';
-import config from '../../config.cjs';
-import { smsg } from '../../lib/myfunc.cjs';
+import config from '../config.cjs';
+import { smsg } from '../lib/myfunc.cjs';
 import { handleAntilink } from './antilink.js';
 import { fileURLToPath } from 'url';
 
