f84d8ed7f4ed7b6a3758685f09546a2b7c11b411
joeljamesXtech <joeljamestech40@gmail.com>
2025-05-11 03:54:51 +0300

    HUNTER-MD-BOT

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,5 +1,6 @@
+import config from '../../config.cjs';
 
-import config from '../config.cjs'; // Ensure this matches your project setup
+const thumbnail = 'https://raw.githubusercontent.com/WISDOMHUNTER01/HUNTER-MD-BOT-/refs/heads/main/media/menu.jpg';
 
 const ping = async (m, sock) => {
   const prefix = config.PREFIX;
@@ -9,35 +10,29 @@ const ping = async (m, sock) => {
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === "ping") {
-    const start = new Date().getTime();
-    await m.React('⏳'); // React with a loading icon
-    const end = new Date().getTime();
+    const start = Date.now();
+    await m.React('⏳');
+    const end = Date.now();
     const responseTime = (end - start).toFixed(2);
 
-    // Updated text style with Sarkar-MD branding and response rate
-    const responseText = `*ᴘᴏɴɢ sᴘᴇᴇᴅ: ${responseTime} ms*`;
+    const responseText = `*ʜᴜɴᴛᴇʀ-ᴍᴅ-ʙᴏᴛ*\n_📶 Response Time:_ *${responseTime} ms*`;
 
-    await m.React('✅'); // React with a success icon
+    await m.React('✅');
 
-    sock.sendMessage(
+    await sock.sendMessage(
       m.from,
       {
         text: responseText,
         contextInfo: {
           isForwarded: true,
-          forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363317462952356@newsletter',
-            newsletterName: "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ",
-            serverMessageId: -1,
-          },
-          forwardingScore: 999, // Score to indicate it has been forwarded
+          forwardingScore: 999,
           externalAdReply: {
-            title: "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
-            body: "ᴘɪɴɢ sᴘᴇᴇᴅ ᴄᴀʟᴄᴜʟᴀᴛɪᴏɴs",
-            thumbnailUrl: 'https://avatars.githubusercontent.com/u/162905644?v=4', // Add thumbnail URL if required
-            sourceUrl: 'https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K', // Add source URL if necessary
+            title: "ʜᴜɴᴛᴇʀ-ᴍᴅ-ʙᴏᴛ ⚡",
+            body: "System Latency Monitor",
+            thumbnailUrl: thumbnail,
+            sourceUrl: "https://github.com/WISDOMHUNTER01/HUNTER-MD-BOT-",
             mediaType: 1,
-            renderLargerThumbnail: false,
+            renderLargerThumbnail: true,
           },
         },
       },
@@ -46,4 +41,5 @@ const ping = async (m, sock) => {
   }
 };
 
+export const command = ['ping'];
 export default ping;
